dea3f8f416a6ade169e7db9cb46e8eb888ceb50c
Nitish Kumar <68236771+nitisharav@users.noreply.github.com>
2026-02-05 05:21:57 +0530

    Implement thread synchronization for odd/even printing
    Ensure that odd and even number threads execute sequentially.

--- OddEvenThread.java
+++ OddEvenThread.java
@@ -16,3 +16,37 @@ class Main {
         }).start();
     }
 }
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+//Ensure one thread finishes before another starts
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+      Thread t1=  new Thread(()->{
+            for(int i=1; i<=10;i+=2){
+                System.out.println(Thread.currentThread().getName()+" : "+i);
+            }
+        });
+      Thread t2=  new Thread(()->{
+            for(int i=0; i<=10;i+=2){
+                System.out.println(Thread.currentThread().getName()+" : "+i);
+            }
+        });
+        t1.start();
+       
+        
+         
+        
+      
+      try {
+          t1.join();
+         
+      } catch(InterruptedException e){
+           
+      }
+       
+         t2.start();
+        
+    }
+}
