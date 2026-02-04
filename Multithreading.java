386be1aa2bb08bddf4b1a3101a7b1b0ea3e71c95
Nitish Kumar <68236771+nitisharav@users.noreply.github.com>
2026-02-05 04:49:15 +0530

    Add multithreading example with Thread class
    Added a second example of multithreading using an anonymous class extending the Thread class.

--- Multithreading.java
+++ Multithreading.java
@@ -2,6 +2,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Try programiz.pro");
+        //using anonymous class implementing runnable interface
         Runnable r= new Runnable(){
             @Override
             public void run(){
@@ -12,5 +13,25 @@ class Main {
         };
         Thread t1= new Thread(r);
         t1.start();
+    }
+}
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        //using anonymous class extending Thread class
+        Thread r= new Thread(){
+            @Override
+            public void run(){
+                for(int i=0;i<5; i++){
+                    System.out.println(Thread.currentThread().getName()+" "+i);
+                }
+            }
+        };
+       // Thread t1= new Thread(r);
+        r.start();
     }
 }
